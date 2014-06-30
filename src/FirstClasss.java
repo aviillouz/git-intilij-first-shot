9eb691085b0779ddd18363d1f1c2d5e87695afeb
aviillouz <avi.illouz@gmail.com>
2014-06-30 14:33:04 +0300

    Initial commit

--- src/FirstClasss.java
+++ src/FirstClasss.java
@@ -1,8 +1,11 @@
 /**
  * Created by Avi on 30/06/14.
+ * A Git try
  */
 public class FirstClasss {
     public static void main(String args[]){
         System.out.println("Hi");
+        System.out.println("It's my first try with git");
+        System.out.println("Hope that it works");
     }
 }
